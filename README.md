181e9e59a86645d497a0e927a1d9db188d496d84
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-09-14 12:19:35 +0300

    Remove edgedb and graphene from python server

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Anonymous voice message board app
 
 ## Helsinki university fullstack course project
 - Check [working hours](docs/workinghours.md) for details on project progress
+
+## Architechture
+- App running on react native
+- Starlette server for filestreams
+- Edgedb database exposed via graphql
--- nois-api/nois_api/main.py
+++ nois-api/nois_api/main.py
@@ -1,31 +1,4 @@
-from graphql.execution.executors.asyncio import AsyncioExecutor
-import graphene
-from starlette.graphql import GraphQLApp
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
 from starlette.routing import Route
 
-from graphql.execution.executors.asyncio import AsyncioExecutor
-from starlette.applications import Starlette
-from starlette.graphql import GraphQLApp
-from starlette.routing import Route
-import graphene
-
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(name=graphene.String(default_value="stranger"))
-
-    async def resolve_hello(self, info, name):
-        # We can make asynchronous network calls here.
-        return "Hello " + name
-
-
-routes = [
-    # We're using `executor_class=AsyncioExecutor` here.
-    Route(
-        "/",
-        GraphQLApp(schema=graphene.Schema(query=Query), executor_class=AsyncioExecutor),
-    )
-]
-
-app = Starlette(routes=routes)
+app = Starlette()
