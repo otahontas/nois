ca24b9cf7243f4f3a0e2e2991dc57bdd5ccd2a59
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-09-16 15:56:48 +0300

    Add theme and icons

--- nois-app/App.tsx
+++ nois-app/App.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import { default as theme } from './theme.json';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { Button, Icon } from '@ui-kitten/components';
+
+const FacebookIcon = (props) => (
+  <Icon name='facebook' {...props} />
+);
+
+export const LoginButton = () => (
+  <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
+);
+
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
     <Text category='h1'>HOME</Text>
+    <LoginButton />
   </Layout>
 );
 
 const App = () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
-  </ApplicationProvider>
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={{...eva.dark, ...theme}}>
+      <HomeScreen />
+    </ApplicationProvider>
+  </>
 );
 
 export default App;
