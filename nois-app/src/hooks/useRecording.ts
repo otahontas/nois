75ada9231cd68ee106057b112af98e2b46ec1e73
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-10-22 22:53:36 +0300

    Add working version of useRecording hook

--- nois-app/src/hooks/useRecording.ts
+++ nois-app/src/hooks/useRecording.ts
@@ -1,17 +1,17 @@
 import { useState } from "react";
 
 import { Audio } from "expo-av";
-// import * as FileSystem from "expo-file-system";
 
 const useRecording = () => {
   const [status, setStatus] = useState<string>("idle");
-  const [error, setError] = useState<any>(null);
-  const [sound, setSound] = useState<Audio.Sound | null>(null);
+  const [error, setError] = useState<string>("");
   const [recording, setRecording] = useState<Audio.Recording | null>(null);
+  const [url, setUrl] = useState<string | null>(null)
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
   const recordingSettings = Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY;
 
   /**
-   * Ask permissions for recording, load recorded into memory and prepare it for
+   * Ask permissions for recording, load recorder into memory and prepare it for
    * recording.
    */
   const prepare = async () => {
@@ -23,51 +23,77 @@ const useRecording = () => {
     return { canRecord, preparedRecording: recordingToPrepare };
   };
 
+  /**
+   * Start recording onto loaded recorder
+   */
   const start = async () => {
-    const { canRecord, preparedRecording } = await prepare();
-    if (!canRecord) return;
     try {
-      const { isRecording } = await preparedRecording.startAsync();
-      console.log(isRecording);
+      const { canRecord, preparedRecording } = await prepare();
+      if (!canRecord) {
+        setStatus("error")
+        setError("Recording is not possible for some unknown reason")
+        return;
+      }
       setStatus("recording");
+      await preparedRecording.startAsync();
       setRecording(preparedRecording);
     } catch (error) {
       setStatus("error");
-      setError(error);
+      setError(error.message);
     }
-    setStatus("recording");
   };
 
+  /**
+   * Stop recording, load recorded message to state and delete recording
+   */
   const stop = async () => {
     try {
-      if (recording) {
-        await recording.stopAndUnloadAsync();
-        setStatus("done");
-        const { sound: _sound } = await recording.createNewLoadedSoundAsync();
-        setSound(_sound);
-        setRecording(null);
-      } else {
-        // do this better
+      if (!recording) {
         setStatus("error");
+        setError("Stopping is not possible since there is no recording available.")
+        return
       }
+      await recording.stopAndUnloadAsync();
+      setStatus("recorded");
+      const { sound: recordedSound } = await recording.createNewLoadedSoundAsync();
+      setSound(recordedSound);
+      setUrl(recording.getURI())
+      setRecording(null);
     } catch (error) {
       setStatus("error");
       setError(error);
     }
   };
 
+  /**
+   * Preview recorded message
+   */
   const preview = async () => {
     try {
-      if (sound) {
-        await sound.playAsync();
+      if (!sound) {
+        console.log("no sound :(")
+        setStatus("error");
+        setError("There is no message to play")
+        return
       }
+      await sound.replayAsync();
     } catch (error) {
       setStatus("error");
       setError(error);
     }
   };
 
-  return [start, stop, preview, status, error];
+  /**
+   * Reset everything to basic state
+   */
+  const reset = async () => {
+    setStatus("idle");
+    setError("");
+    setRecording(null);
+    setUrl(null);
+    setSound(null);
+  }
+  return [start, stop, preview, status, error, url, reset];
 };
 
 export default useRecording;
