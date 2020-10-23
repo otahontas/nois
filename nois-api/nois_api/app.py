2572a4c78094d1e4edebdfc419f23557fea12d39
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-10-23 13:35:03 +0300

    Add initial version of ariadne graphql app

--- nois-api/nois_api/app.py
+++ nois-api/nois_api/app.py
@@ -1,19 +1,28 @@
+from ariadne import QueryType, make_executable_schema
+from ariadne.asgi import GraphQL
 from starlette.applications import Starlette
-from starlette.routing import Route, Mount
-
-from .gino import db
-from .graphql import graphql_app
-from .file_api import file_api_routes, init_file_api_folder
+from starlette.routing import Route
 
 from .config import DEBUG
 
-routes = [
-    Route("/graphql", graphql_app, name="graphql"),
-    Mount("/files", routes=file_api_routes, name="files"),
-]
+type_defs = """
+    type Query {
+        hello: String!
+    }
+"""
+query = QueryType()
+
+
+@query.field("hello")
+def resolve_hello(*_):
+    return "Hello world!"
+
+
+schema = make_executable_schema(type_defs, query)
+
+routes = [Route("/graphql", GraphQL(schema, debug=DEBUG))]
 
 
 def get_app():
-    app = Starlette(routes=routes, on_startup=[init_file_api_folder], debug=DEBUG)
-    db.init_app(app)
+    app = Starlette(routes=routes, debug=DEBUG)
     return app
--- nois-api/nois_api/config.py
+++ nois-api/nois_api/config.py
@@ -1,4 +1,4 @@
-from sqlalchemy.engine.url import URL, make_url
+# from sqlalchemy.engine.url import URL, make_url
 from starlette.config import Config
 from starlette.datastructures import Secret
 
@@ -11,44 +11,44 @@ TESTING = config("TESTING", cast=bool, default=False)
 DEBUG = config("DEBUG", cast=bool, default=False)
 
 # Database
-DB_DRIVER = config("DB_DRIVER", default="postgresql")
-DB_HOST = config("DB_HOST", default=None)
-DB_PORT = config("DB_PORT", cast=int, default=None)
-DB_DATABASE = "nois_test" if TESTING else config("DB_DATABASE", default=None)
-DB_USER = "nois_test" if TESTING else config("DB_USER", default=None)
-DB_PASSWORD = (
-    "nois_test" if TESTING else config("DB_PASSWORD", cast=Secret, default=None)
-)
-DB_DSN = config(
-    "DB_DSN",
-    cast=make_url,
-    default=URL(
-        drivername=DB_DRIVER,
-        username=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
-        database=DB_DATABASE,
-    ),
-)
-DB_POOL_MIN_SIZE = config("DB_POOL_MIN_SIZE", cast=int, default=1)
-DB_POOL_MAX_SIZE = config("DB_POOL_MAX_SIZE", cast=int, default=16)
-DB_ECHO = config("DB_ECHO", cast=bool, default=False)
-DB_SSL = config("DB_SSL", default=None)
-DB_USE_CONNECTION_FOR_REQUEST = config(
-    "DB_USE_CONNECTION_FOR_REQUEST", cast=bool, default=True
-)
-DB_RETRY_LIMIT = config("DB_RETRY_LIMIT", cast=int, default=1)
-DB_RETRY_INTERVAL = config("DB_RETRY_INTERVAL", cast=int, default=1)
-
-# File API
-FILE_API_FOLDER = config(
-    "FILE_API_FOLDER", default=Path(__file__).parent.parent.absolute() / "files"
-)
-FILE_API_DEFAULT_CHUNK_SIZE = config(
-    "FILE_API_DEFAULT_BLOCKSIZE", cast=int, default=1024
-)
-FILE_API_BASE_URL = config("FILE_API_BASE_URL", cast=str, default="/files")
-
-# Graphql API
-GRAPHQL_API_BASE_URL = config("GRAPHQL_API_BASE_URL", cast=str, default="/graphql")
+# DB_DRIVER = config("DB_DRIVER", default="postgresql")
+# DB_HOST = config("DB_HOST", default=None)
+# DB_PORT = config("DB_PORT", cast=int, default=None)
+# DB_DATABASE = "nois_test" if TESTING else config("DB_DATABASE", default=None)
+# DB_USER = "nois_test" if TESTING else config("DB_USER", default=None)
+# DB_PASSWORD = (
+#     "nois_test" if TESTING else config("DB_PASSWORD", cast=Secret, default=None)
+# )
+# DB_DSN = config(
+#     "DB_DSN",
+#     cast=make_url,
+#     default=URL(
+#         drivername=DB_DRIVER,
+#         username=DB_USER,
+#         password=DB_PASSWORD,
+#         host=DB_HOST,
+#         port=DB_PORT,
+#         database=DB_DATABASE,
+#     ),
+# )
+# DB_POOL_MIN_SIZE = config("DB_POOL_MIN_SIZE", cast=int, default=1)
+# DB_POOL_MAX_SIZE = config("DB_POOL_MAX_SIZE", cast=int, default=16)
+# DB_ECHO = config("DB_ECHO", cast=bool, default=False)
+# DB_SSL = config("DB_SSL", default=None)
+# DB_USE_CONNECTION_FOR_REQUEST = config(
+#     "DB_USE_CONNECTION_FOR_REQUEST", cast=bool, default=True
+# )
+# DB_RETRY_LIMIT = config("DB_RETRY_LIMIT", cast=int, default=1)
+# DB_RETRY_INTERVAL = config("DB_RETRY_INTERVAL", cast=int, default=1)
+# 
+# # File API
+# FILE_API_FOLDER = config(
+#     "FILE_API_FOLDER", default=Path(__file__).parent.parent.absolute() / "files"
+# )
+# FILE_API_DEFAULT_CHUNK_SIZE = config(
+#     "FILE_API_DEFAULT_BLOCKSIZE", cast=int, default=1024
+# )
+# FILE_API_BASE_URL = config("FILE_API_BASE_URL", cast=str, default="/files")
+# 
+# # Graphql API
+# GRAPHQL_API_BASE_URL = config("GRAPHQL_API_BASE_URL", cast=str, default="/graphql")
