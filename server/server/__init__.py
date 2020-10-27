3c7986dea7bf44e6da134378d88d40aef2187c9b
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-10-27 11:45:45 +0200

    Add file folder initialization when server starts

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,23 +1,18 @@
 from starlette.applications import Starlette
 from starlette.routing import Route, Mount
-from starlette.responses import JSONResponse
 from server.graphql import graphql_app
 from server.api import api_routes
 
 from server.config import DEBUG
 
-from server.database import init_db, create_pool, close_pool
-
-
-async def homepage(request):
-    return JSONResponse({"hello": "world"})
-
+from server.database import db
+from server.file_io import init_file_api_folder
 
 routes = [Route("/graphql", graphql_app), Mount("/api", routes=api_routes)]
 
 app = Starlette(
     routes=routes,
     debug=DEBUG,
-    on_startup=[init_db, create_pool],
-    on_shutdown=[close_pool],
+    on_startup=[db.initialize_database, init_file_api_folder],
+    on_shutdown=[db.close_pool],
 )
